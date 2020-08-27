4d4ff5494cb03c9198d4aeac3402e6825b37c0d1
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-26 18:12:24 -0700

    Bugs

--- SpaceDefenderV3/Assets/LaserScript.cs
+++ SpaceDefenderV3/Assets/LaserScript.cs
@@ -6,12 +6,13 @@ public class LaserScript : MonoBehaviour
 {
     private Rigidbody2D RB;
     public float Speed;
-    //public Transform ShootPoint;
+    private Transform Arm;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        Arm = GameObject.FindGameObjectWithTag("Arm").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -24,8 +25,12 @@ public class LaserScript : MonoBehaviour
     {
         //Vector2 Direction = new Vector2(ShootPoint.position.x - transform.position.x, ShootPoint.position.y - transform.position.y);
 
-        //transform.Translate(Vector2.right * Speed * Time.deltaTime);
+        //transform.Translate(Arm.position * Speed * Time.deltaTime);
+
+        //transform.Translate(Vector3.right * Speed * Time.deltaTime);
 
         RB.velocity = Vector3.right * Speed * Time.deltaTime;
+
+        //RB.AddForce(new Vector2(Arm.rotation * Speed, Arm.rotation * Speed));
     }
 }
