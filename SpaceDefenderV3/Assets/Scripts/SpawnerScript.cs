fbe463fcb1d0a58540119c2dd9101e4c521cffa4
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-27 17:19:44 -0700

    Ship Added
    This is the default version now

---
+++ SpaceDefenderV3/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public Transform[] EnemyShips;
+    public float Timer;
+    public int RandShip;
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(Timer);
+        Instantiate(EnemyShips[RandShip], transform.position, transform.rotation);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+}
