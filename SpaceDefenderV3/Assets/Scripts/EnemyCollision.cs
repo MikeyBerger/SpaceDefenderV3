b540aeffac4fe8a3c5315539cbf31e390fa8c885
MikeyBerger <michaeljudahberger@gmail.com>
2020-08-31 17:24:44 -0700

    Failed Attempt at Fixing Bugs
    Do not download EVER!!

--- SpaceDefenderV3/Assets/Scripts/EnemyCollision.cs
+++ SpaceDefenderV3/Assets/Scripts/EnemyCollision.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
 {
+    public bool WasHit = false;
+    //private Transform Graphic;
+    //public Material Material;
+    //private SpriteRenderer SR;
 
+    private void Start()
+    {
+        //Graphic.GetComponent<SpriteRenderer>();
+        //SR = GetComponentInChildren<SpriteRenderer>();
+        //SR.enabled = true;
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,8 +25,15 @@ public class EnemyCollision : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Instantiate(Explosion, transform.position, transform.rotation);
-        Destroy(transform.gameObject);
-        Destroy(collision.gameObject);
+        if (collision.gameObject.tag == "Laser")
+        {
+            //Destroy(transform.gameObject);
+            WasHit = true;
+            //SR.material = Material;
+            Destroy(collision.gameObject);
+        }
+        //Destroy(transform.gameObject);
+        //Destroy(collision.gameObject);
         
     }
 
